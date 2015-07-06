cc6533f50cf3cba67d046ce210afe96428aa919d
beradrian <beradrian@yahoo.com>
2015-07-06 23:23:22 +0300

    make it work with angular use npm

---
+++ jsbandwidth.js
@@ -0,0 +1,119 @@
+var JsBandwidth = function($http) {
+	this.DEFAULT_OPTIONS = {
+		downloadUrl: ""
+		, uploadUrl: ""
+		, uploadDataSize: 5 * 1024 * 1024
+	};
+	
+	if (angular) {
+		this.extend = angular.extend;
+		var self = this;
+		angular.module("jsBandwidth", [])
+				.factory("jsBandwidth", ["$http", "$q", function($http, $q) {
+					self.deferredConstructor = $q.defer;
+					self.$http = $http;
+					self.ajax = $http;			
+					return self;
+				}]);
+	} else if (jQuery) {
+		this.extend = jQuery.extend;
+		this.deferredConstructor = jQuery.Deferred;
+		this.ajax = jQuery.ajax;
+	} else {
+		throw "Either Angular or jQuery is mandatory for JsBandwidth";
+	}
+};
+
+JsBandwidth.prototype.config = function(options) {
+	this.DEFAULT_OPTIONS = options;
+};
+
+/**
+ * Calculates the bandwidth in Mbps
+ * @param size the size in bytes to be transfered
+ * @param startTime the time when the transfer started. The end time is 
+ * considered to be now.
+ */
+JsBandwidth.prototype.calculateBandwidth = function(size, start) {
+	var end = new Date().getTime();
+	var diff = (end - start) / 1000;
+	var speed = (size / diff) / 1024 / 1024 * 8;
+	speed = Math.round(speed * 100) / 100;
+	return speed;
+};
+
+JsBandwidth.prototype.testDownloadSpeed = function(options) {
+	var self = this;
+	options = this.extend({}, this.DEFAULT_OPTIONS, options);
+	var deferred = this.deferredConstructor();
+	var start = new Date().getTime();
+	this.ajax({
+			method: "GET",
+			url: options.downloadUrl + "?id=" + start,
+			dataType: 'application/octet-stream',
+			headers: {'Content-type': 'application/octet-stream'}})
+			.then(
+				function(response) {
+					deferred.resolve({downloadSpeed: self.calculateBandwidth((response.data || response).length, start), data: response.data || response});
+				},
+				function(response) {
+					deferred.reject(response.error || response);
+				}
+			);
+	return deferred.promise;
+};
+
+JsBandwidth.prototype.testUploadSpeed = function(options) {
+	var self = this;
+	options = this.extend({}, this.DEFAULT_OPTIONS, options);
+	// generate randomly the upload data
+	if (!options.uploadData) {
+		options.uploadData = new Array(options.uploadDataSize);
+		for (var i = 0; i < options.uploadData.length; i++) {
+			options.uploadData[i] = Math.floor(Math.random() * 256);
+		}
+	}
+	var deferred = this.deferredConstructor();
+	var start = new Date().getTime();
+	this.ajax({
+			method: "POST",
+			url: options.uploadUrl + "?id=" + start,
+			data: options.uploadData,
+			dataType: 'application/octet-stream',
+			headers: {'Content-type': 'application/octet-stream'}})
+			.then(
+				function(response) {
+					deferred.resolve({uploadSpeed: self.calculateBandwidth(options.uploadData.length, start)});
+				}
+				, function(response) {
+					deferred.reject(response.error || response);
+				}    
+			);
+	return deferred.promise;
+};
+
+JsBandwidth.prototype.testSpeed = function(options) {
+	var self = this;
+	var deferred = this.deferredConstructor();
+	self.testDownloadSpeed(options)
+			.then(function(downloadResult) {
+				options.uploadData = downloadResult.data;
+				self.testUploadSpeed(options)
+						.then(
+							function(uploadResult) {
+								deferred.resolve({downloadSpeed: downloadResult.downloadSpeed, uploadSpeed: uploadResult.uploadSpeed});
+							}
+							, function(response) {
+								deferred.reject(response.error || response);
+							}
+						);
+				}
+				, function(response) {
+					deferred.reject(response.error || response);
+				}
+			);
+	return deferred.promise;
+};
+
+if (!module) module = {};
+module.exports = new JsBandwidth();
---
+++ test-jsbandwidth.js
@@ -0,0 +1,97 @@
+var testApp = angular.module('JsBandwidthTestApp', ["jsBandwidth"]);
+/*
+testApp.config(function($provide) {
+    $provide.decorator('$httpBackend', function($delegate) {
+        var proxy = function(method, url, data, callback, headers) {
+            var interceptor = function() {
+                var _this = this,
+                    _arguments = arguments;
+                setTimeout(function() {
+                    callback.apply(_this, _arguments);
+                }, 1000);
+            };
+            return $delegate.call(this, method, url, data, interceptor, headers);
+        };
+        for(var key in $delegate) {
+            proxy[key] = $delegate[key];
+        }
+        return proxy;
+    });
+});
+*/
+
+var prefix = "http://localhost:8081/";
+
+testApp.controller('JsBandwidthTestController', ["$scope", "jsBandwidth", function ($scope, jsBandwidth) {
+	$scope.test = function(options, callback) {
+		jsBandwidth.testSpeed(options)
+				.then(function(result) {
+						$scope.downloadSpeed = result.downloadSpeed;
+						$scope.uploadSpeed = result.uploadSpeed;
+						$scope.errorStatus = null;
+						callback();
+					}
+					, function(error) {
+						$scope.downloadSpeed = -1;
+						$scope.uploadSpeed = -1;
+						$scope.errorStatus = error.status;
+						callback();
+					});
+	};
+}]);
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+
+describe('JsBandwidthTestController', function() {
+	var $httpBackend, $rootScope, createController, authRequestHandler;
+
+	// Set up the module
+	beforeEach(module('JsBandwidthTestApp'));
+
+	beforeEach(inject(function($injector) {
+		// Set up the mock http service responses
+		$httpBackend = $injector.get('$httpBackend');
+
+		// Get hold of a scope (i.e. the root scope)
+		$rootScope = $injector.get('$rootScope');
+		
+		// The $controller service is used to create instances of controllers
+		var $controller = $injector.get('$controller');
+
+		createController = function() {
+			return $controller('JsBandwidthTestController', {'$scope' : $rootScope });
+		};
+	}));
+
+	afterEach(function() {
+         $httpBackend.verifyNoOutstandingExpectation();
+         $httpBackend.verifyNoOutstandingRequest();
+	});
+
+	it('should get net speed', function(done) {
+		var dataSize = 1024 * 1024;
+		var data = new Array(dataSize);
+		for (var i = 0; i < data.length; i++) {
+			data[i] = Math.floor(Math.random() * 256);
+		}
+		$httpBackend.when('GET', /(.+\/)?test\.bin(\?.+)?/).respond(200, data, {"Access-Control-Allow-Origin": "*"});
+		$httpBackend.when('POST', /(.+\/)?post(\?.+)?/).respond(200, '', {"Access-Control-Allow-Origin": "*"});
+		createController();
+		$rootScope.test({downloadUrl: prefix + "test.bin", uploadUrl: prefix + "post"}, function() {
+			expect($rootScope.downloadSpeed).toBeLessThan(1);
+			done();
+		});
+		setTimeout(function() {
+		$httpBackend.flush();
+		}, (dataSize / (1024 * 1024)) * 8 * 1000 + 100);
+	});
+
+	it('should get error', function() {
+		$httpBackend.when('GET', /(.+\/)?test\.binx(\?.+)?/).respond(404, '', {"Access-Control-Allow-Origin": "*"});
+		createController();
+		$rootScope.test({downloadUrl: prefix + "test.binx", uploadUrl: prefix + "post"}, function() {
+			expect($rootScope.errorStatus).toEqual(404);
+		});
+		$httpBackend.flush();
+	});
+});
